aec0199287161cd881f0bf0234ed2f0e15fd3e1d
Dem2015 <103990047+Dem2015@users.noreply.github.com>
2022-08-01 11:54:19 +0500

    Cooks - Home Work of Dem2014

--- java.js
+++ java.js
@@ -4,18 +4,28 @@ $(document).ready(function() {
     $('.slide-container.slide1').addClass("active");
     $(".slide-container.slide2").removeClass("active");
     $(".slide-container.slide3").removeClass("active");
+    $('body').addClass("bg1");
+    $("body").removeClass("bg2");
+    $("body").removeClass("bg3");
     return;
   });
   $(".slider-trigger-2").click(function () {
     $(".slide-container.slide1").removeClass("active");
     $(".slide-container.slide3").removeClass("active");
     $('.slide-container.slide2').addClass("active");
+    $('.slide-container.slide2').addClass("active");
+    $('body').addClass("bg2");
+    $("body").removeClass("bg1");
+    $("body").removeClass("bg3");
     return;
   });
   $(".slider-trigger-3").click(function () {
     $(".slide-container.slide1").removeClass("active");
     $(".slide-container.slide2").removeClass("active");
     $('.slide-container.slide3').addClass("active");
+    $('body').addClass("bg3");
+    $("body").removeClass("bg1");
+    $("body").removeClass("bg2");
     return;
   });
 });
